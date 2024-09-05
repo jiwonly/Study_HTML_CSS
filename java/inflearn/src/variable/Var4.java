5c8a4d545ae95aa3498868d225dbbefc6fd8e517
Jun Jiwon <ysjjw2003@naver.com>
2024-09-05 22:30:26 +0900

    add var & operator

---
+++ java/inflearn/src/variable/Var4.java
@@ -0,0 +1,19 @@
+package variable;
+
+public class Var4 {
+    public static void main(String[] args) {
+        //정수
+        byte b =127; //-128 ~ 127
+        short s = 32767; // -32768 ~ 32767
+        int i = 2147483647; // -2147483648 ~ 2147483647 (약 20억)
+
+        long l =9223372036854775807L;
+
+        //실수
+        float f =10.0f;
+        double d =10.0;
+
+
+
+    }
+}
